80fbd90ce6f6c759ebea94b0427d035c8210ad0f
Alyona Golz <emax@coex.tech>
2023-01-18 01:02:40 +0400

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,4 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*buf;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	buf = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (buf == NULL)
+		return (NULL);
+	ft_strcpy(buf, s1);
+	ft_strcat(buf, s2);
+	return (buf);
+}
