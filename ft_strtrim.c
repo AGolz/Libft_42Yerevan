ca6f3e5f2125af263f988796c51c158cfac9aada
Alyona Golz <emax@coex.tech>
2023-01-18 01:04:57 +0400

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,3 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		len;
+	char	*str;
+
+	if (!s1)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1)
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strchr(set, s1[len]) && len)
+		--len;
+	str = ft_substr(s1, 0, (len + 1));
+	return (str);
+}
