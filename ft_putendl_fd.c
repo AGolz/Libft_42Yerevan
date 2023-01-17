468c3ee0726210794b09fef33dcc12598e14b7f4
Alyona Golz <emax@coex.tech>
2023-01-18 00:55:25 +0400

    Update ft_putendl_fd.c

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,3 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (fd < 0 || !s)
+		return ;
+	ft_putstr_fd(s, fd);
+	write(fd, "\n", 1);
+}
