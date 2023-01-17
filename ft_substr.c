5ef06976fc697265f7071ef66aecd4cf2d7a2e5e
Alyona Golz <emax@coex.tech>
2023-01-18 01:05:58 +0400

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -10,4 +10,31 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*dst;
+	size_t	s_len;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		start = s_len;
+	if (len > s_len - start)
+		len = s_len - start;
+	dst = malloc(len + 1);
+	if (!dst)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		dst[i] = s[start];
+		i++;
+		start++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
