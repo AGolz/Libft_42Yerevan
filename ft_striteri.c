3041b8e46c3aa710f572e636e09b6a1284d3d1dd
Elena Maksimova <emaksimo@c2r13s2.42yerevan.am>
2023-01-18 01:12:51 +0400

    striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,22 @@
 /*   By: emaksimo <emaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 00:50:01 by emaksimo          #+#    #+#             */
-/*   Updated: 2023/01/18 00:50:02 by emaksimo         ###   ########.fr       */
+/*   Updated: 2023/01/18 01:11:52 by emaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned int i;
+
+	if (!s || !f)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
+}
